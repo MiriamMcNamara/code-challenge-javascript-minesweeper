5ead977cb75d8adc885bf5bde3e02b080400224a
Miriam McNamara <miriammcnamara@icloud.com>
2022-01-17 15:21:09 -0600

    first bit of logic working

--- minesweeper.js
+++ minesweeper.js
@@ -8,88 +8,61 @@
 const annotate = (inputArray) => {
   console.log("inputArray:", inputArray);
 
-  //Pseudocode brainstorm:
-  //reminder: console.log each step, check each one along the way!
-
   //create an array to return, returnArray
   let returnArray = [];
   //loop through the inputArray
   for (let i = 0; i < inputArray.length; i++) {
-    //add IFs for edge cases here
+    //add IFs for edge cases here?
 
     //declare variable for the inputString
     let inputString = inputArray[i];
     console.log("inputString:", inputString);
 
-    //create a variable for the new string here? returnString
+    //create a variable for the returnString
     let returnString = "";
 
     //loop through each inputString in the inputArray
     for (let j = 0; j < inputString.length; j++) {
-      //add IFs for edge cases here
-      //create a variable for the space you are working in, currentSpace=0
+      //add IFs for edge cases here?
+      //create a variable for the space you are working in
       let currentSpace = 0;
-      if (inputString[j] === "5") {
+      console.log("currentSpace:", currentSpace);
+      //first check iF the character you are at is an asterisk, then currentSpace="*"
+      if (inputString[j] === "*") {
         currentSpace = "*";
+        console.log("currentSpace asterisk:", currentSpace);
       } else {
-        currentSpace = inputString[j];
+        // if ((inputString[j - 1] = !null)) { (not sure we need this?)
+        if (inputString[j - 1] === "*") {
+          currentSpace++;
+          console.log("currentSpace increment:", currentSpace);
+        }
+        // }
+
+        //inputArray[i].inputString[j] check if these are asterisks:
+
+        //if each of these things EXISTS, do a thing (nest some ifs? would that take care of edges?)
+        //inputArray[i].string[j-1, j+1]
+        //inputArray[i-1].string[j, j+1, j-1]
+        //inputArray[i+1].string[j, j+1, j-1]
+
+        //IF the space we are checking against is an asterisk, currentSpace ++
       }
-      //first check iF the character you are at is an asterisk, currentSpace='/*' (will this work in javascript? i think so)
-      //concatenate currentSpace onto returnString, move to next spot
-      returnString = returnString + currentSpace;
+      if (currentSpace === 0) {
+        returnString = returnString + " ";
+      } else {
+        //concatenate currentSpace onto returnString (might need to toString?), move to next spot
+        returnString = returnString + currentSpace;
+      }
 
-      //for each character that IS NOT an edge piece OR an asterisk, this should work:
-      //array[i].string[j] check if these are asterisks:
-      //array[i].string[j-1, j+1]
-      //array[i-1].string[j, j+1, j-1]
-      //array[i+1].string[j, j+1, j-1]
-
-      //IF it's an asterisk, currentSpace ++
-
-      // after you check all of them, concatenate currentSpace onto returnString
-      //move to next currentSpace
-      //once all the currentSpaces have been checked in each string, push the returnString into the returnArray
-
-      //if this doesn't work against edge pieces, i'll have to customize a bit further
-
-      //at the end, toString to ensure the numbers are all returned to string?
+      //if this doesn't work against edge pieces, i'll have to customize a bit further...
     } //end j loop
     console.log("returnString:", returnString);
+    //once all the currentSpaces have been checked in each string, push the returnString into the returnArray
     returnArray.push(returnString);
   } //end i loop
   console.log("returnArray:", returnArray);
   //return the returnArray
 };
 
-annotate(["hel5o", "wor5d"]);
-
-//Pseudocode brainstorm:
-//reminder: console.log each step, check each one along the way!
-
-//create an array to return, returnArray
-//loop through the array
-//add IFs for edge cases here
-//create a variable for the new string here? returnString
-//loop through each string
-//add IFs for edge cases here
-//create a variable for the space you are working in, currentSpace=0
-//first check iF the character you are at is an asterisk, currentSpace='/*' (will this work in javascript? i think so)
-//concatenate currentSpace onto returnString, move to next spot
-
-//for each character that IS NOT an edge piece OR an asterisk, this should work:
-//array[i].string[j] check if these are asterisks:
-//array[i].string[j-1, j+1]
-//array[i-1].string[j, j+1, j-1]
-//array[i+1].string[j, j+1, j-1]
-
-//IF it's an asterisk, currentSpace ++
-
-// after you check all of them, concatenate currentSpace onto returnString
-//move to next currentSpace
-//once all the currentSpaces have been checked in each string, push the returnString into the returnArray
-
-//if this doesn't work against edge pieces, i'll have to customize a bit further
-
-//at the end, toString to ensure the numbers are all returned to string?
-
-//return the returnArray
+annotate(["*  ", " * "]);
